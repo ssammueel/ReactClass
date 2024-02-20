aa9d4aedd0956187d9e6536a1f62ff4d8879cbd9
samuel <124765323+ssammueel@users.noreply.github.com>
2024-02-20 23:17:35 +0300

    Add files via upload

---
+++ DAY 2/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Product from './Product'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>hellows</h1>
+      <Product/>
+
+    </div>
+    
+  )
+}
+
+export default App
---
+++ DAY 2/Product.jsx
@@ -0,0 +1,10 @@
+const Product = () => {
+  return (
+    <div className="products">
+        <h1>laptop</h1>
+        <h3>thinpad</h3>
+    </div>
+  )
+}
+
+export default Product
