7957ee31159484652f22a1404650cc5a566e5dc2
samuel <124765323+ssammueel@users.noreply.github.com>
2024-02-21 00:06:50 +0300

    School.jsx

--- DAY 2/Product.jsx
+++ DAY 2/Product.jsx
@@ -1,10 +1,31 @@
-const Product = () => {
+import { useState } from "react"
+import { useEffect } from "react"
+
+
+const School = () => {
+
+    let [student, setStudent] = useState("samuel")
+    let[age, setAge] = useState(20)
+    
+    useEffect(()=>{
+        console.log("mounted sucessful")
+    })
+
+
   return (
     <div className="products">
-        <h1>laptop</h1>
-        <h3>thinpad</h3>
+        <h1>{student}</h1>
+
+        <button onClick={()=>{
+            setStudent("purity")
+        }}>change student</button>
+
+        <button onClick={()=>{
+            setAge(29);
+        }}>change age</button>
+        
     </div>
   )
 }
 
-export default Product
+export default School;
