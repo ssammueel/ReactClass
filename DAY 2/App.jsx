bac441d484847944ef6815ca6325d7120638f94a
samuel <124765323+ssammueel@users.noreply.github.com>
2024-02-21 11:36:40 +0300

    App.jsx

--- DAY 2/App.jsx
+++ DAY 2/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react'
 import './App.css'
-import Product from './Product'
+import School from './School'
 
 function App() {
 
-
+let [showStudent, setShowStudent] = useState(true)
   return (
     <div>
-      <h1>hellows</h1>
-      <Product/>
+      <h1>hellows samuel</h1>
+
+      <button onClick={()=>{
+        setShowStudent(true)
+      }}>show</button>
+
+      <button onClick={()=>{
+        setShowStudent(false)
+      }}>hide</button>
+
+    {
+
+      showStudent === true?
+      (
+        <School/>
+      ):
+      <h1> products not availabe </h1>
+
+    }
 
     </div>
     
