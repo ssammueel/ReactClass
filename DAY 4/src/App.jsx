7c2c2f9e7031e27c0395cd288ff157c3d5e6730f
samuel <124765323+ssammueel@users.noreply.github.com>
2024-02-23 09:34:56 +0300

    App.jsx

--- DAY 4/src/App.jsx
+++ DAY 4/src/App.jsx
@@ -1,10 +1,42 @@
 import './App.css'
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Product from './components/Product'
+import Service from './components/Servise'
+
+import Header from './components/Header'
+
+import Image from './components/about/Image'
+import Price from './components/about/Price'
+import Location from './components/about/Location'
+import Refdemo from './components/Refdemo'
 
 function App() {
  
   return (
     <>
-      <h1>sprdfdfdf </h1>
+
+    {/* <Refdemo/> */}
+
+     <BrowserRouter>
+        <Header/>
+     <Routes>
+
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}>
+        <Route path='image' element={<Image/>}></Route>
+        <Route path='price' element={<Price/>}></Route>
+        <Route path='location' element={<Location/>}></Route>
+
+      </Route>
+      <Route path='/product' element= {<Product/>}/>
+      <Route path='/service' element={<Service/>} />
+
+     </Routes>
+     
+     </BrowserRouter>
+
     </>
   )
 }
