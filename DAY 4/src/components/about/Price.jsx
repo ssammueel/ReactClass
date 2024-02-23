8719bd3dca861505ea7248bdbacb1ab7e5e6659c
samuel <124765323+ssammueel@users.noreply.github.com>
2024-02-23 10:34:00 +0300

    Price.jsx

--- DAY 4/src/components/about/Price.jsx
+++ DAY 4/src/components/about/Price.jsx
@@ -2,6 +2,12 @@ const Price = () => {
   return (
     <>
     <h2>the price</h2>
+    <div className="img">
+      <div className="i">Price 1</div>
+      <div className="i">Price 2</div>
+      <div className="i">Price 3</div>
+      <div className="i">Price 4  </div>
+    </div>
     </>
   )
 }
