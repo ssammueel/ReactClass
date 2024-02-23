15af18df5b3c59750cb594e4c79372a22e44007e
samuel <124765323+ssammueel@users.noreply.github.com>
2024-02-24 01:28:39 +0300

    App.jsx

--- DAY 5/App.jsx
+++ DAY 5/App.jsx
@@ -2,24 +2,27 @@
 import { useState,createContext} from 'react'
 import './App.css'
 import Pproduct from './Pproduct'
-import School from './School'
-import Student from './Student'
-import Course from './Course'
 
-export const Toexport = createContext()
+export const Toexport = createContext() 
 
 function App() {
 
-  return(
+  let [userName, setUserName] = useState("samuel")
+  let[surName, setSurName] = useState("muthama")
+
+  return (
     <>
-    <div className="app">
-      <h1> the app component</h1>
+    {/* <Toexport.Provider value={userName}> // exporting a sinle component*/}
+
+    <Toexport.Provider value={{userName,surName,setUserName}}>
+    {/* {userName,surName} this is the same as {userName:"samuel",surName="muthama"} */}
+      <h1>app component <br></br>{userName} {surName}</h1>
+      <Pproduct/>
+
+    </Toexport.Provider>
+
       
-    </div>
-    <School/>
-      <Student/>
-      <Course/>
-    </>
+      </>
   )
   
   
@@ -27,19 +30,3 @@ function App() {
 
 export default App
  
-// let [userName, setUserName] = useState("samuel")
-//   let[surName, setSurName] = useState("muthama")
-
-//   return (
-//     <>
-//     {/* <Toexport.Provider value={userName}> // exporting a sinle component*/}
-
-//     <Toexport.Provider value={{userName,surName,setUserName}}>
-//     {/* {userName,surName} this is the same as {userName:"samuel",surName="muthama"} */}
-//       <h1>app component <br></br>{userName} {surName}</h1>
-//       <Pproduct/>
-
-//     </Toexport.Provider>
-
-      
-//       </>
